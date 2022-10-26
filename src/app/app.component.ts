127311539b2b981d8b103b0a1f81c25a3119a367
Dawid Dlugosz <dawid.dlugosz@gmail.com>
2022-10-26 19:07:14 +0200

    1.1 - Inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <p><h1>Oto moja strona w angularze jako template z componentu.</h1><br/>
+  <p><h2>Drugi paragraf z tego samego componentu</h2>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
